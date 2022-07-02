7d9f38f5847558d1539126104e149bd6749a2d40
Niklas Kariniemi <niklaskariniemi@gmail.com>
2022-07-02 12:18:11 +0300

    Add initial script

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
